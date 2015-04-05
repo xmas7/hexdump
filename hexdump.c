218ce7940710862668cec63269f7b05d0a07c146
xmas7 <xhunter0228@gmail.com>
2015-04-05 16:02:43 +0300

    Drop unneeded header

--- hexdump.c
+++ hexdump.c
@@ -3,12 +3,10 @@
 #include <errno.h>
 
 #include <sys/mman.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include <unistd.h>
